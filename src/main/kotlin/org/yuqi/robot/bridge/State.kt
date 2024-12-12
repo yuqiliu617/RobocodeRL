9ea923b34ddd2118f1ae9b187ccdc6a4af0f6ee1
Yuqi Liu <yuqiliu617@outlook.com>
2024-12-12 06:54:34 -0800

    feat: change State presentation
    Change order of dimensions and add new dimension op.hitwall.

--- src/main/kotlin/org/yuqi/robot/bridge/State.kt
+++ src/main/kotlin/org/yuqi/robot/bridge/State.kt
@@ -28,8 +28,8 @@ open class RawState(
         }
     }
 
-    val reward: Float
-        get() {
+    val reward
+        get(): Float {
             events.firstOrNull { it is RoundEndedEvent }?.let {
                 val ev = it as RoundEndedEvent
                 val base = when {
@@ -61,47 +61,45 @@ open class RawState(
             return r.toFloat()
         }
 
-    val score: Float
-        get() {
-            events.firstOrNull { it is RoundEndedEvent }?.let {
-                (if (status.energy > 0.0) 50F else 0F) +
-                    if (status.opponentsCount == 0) 10F else 0F
-            }
-            var s = events.sumOf {
-                when (it) {
-                    is BulletHitEvent -> {
-                        totalBulletDamage += it.bullet.damage
-                        it.bullet.damage + (if (it.energy <= 0.0) totalBulletDamage * 0.2 else 0.0)
-                    }
-
-                    is HitRobotEvent  -> {
-                        totalRamDamage += Rules.ROBOT_HIT_DAMAGE
-                        Rules.ROBOT_HIT_DAMAGE * 2 + (if (it.energy <= 0.0) totalRamDamage * 0.3 else 0.0)
-                    }
-
-                    else              -> 0.0
-                }
-            }
-            return s.toFloat()
+    fun getScore(): Float {
+        events.firstOrNull { it is RoundEndedEvent }?.let {
+            (if (status.energy > 0.0) 50F else 0F) +
+                if (status.opponentsCount == 0) 10F else 0F
         }
+        return events.sumOf {
+            when (it) {
+                is BulletHitEvent -> {
+                    totalBulletDamage += it.bullet.damage
+                    it.bullet.damage + (if (it.energy <= 0.0) totalBulletDamage * 0.2 else 0.0)
+                }
 
-    fun distanceToWall(dir: Direction): Double = with(robot) {
+                is HitRobotEvent  -> {
+                    totalRamDamage += Rules.ROBOT_HIT_DAMAGE
+                    Rules.ROBOT_HIT_DAMAGE * 2 + (if (it.energy <= 0.0) totalRamDamage * 0.3 else 0.0)
+                }
+
+                else              -> 0.0
+            }
+        }.toFloat()
+    }
+
+    fun distanceToWalls(p: Point = status.position): WallDistance = with(robot) {
         val radius = sqrt(2.0) / 2 * width
-        return when (dir) {
-            Direction.Up    -> battleFieldHeight - y
-            Direction.Down  -> y
-            Direction.Left  -> x
-            Direction.Right -> battleFieldWidth - x
-        } - radius
+        return WallDistance(
+            battleFieldHeight - p.y - radius,
+            p.y - radius,
+            p.x - radius,
+            battleFieldWidth - p.x - radius
+        )
     }
 }
 
 class State(
-    robot: AdvancedRobot,
+    robot: Robot,
     status: Status,
-    targetStatus: OpponentStatus,
+    opponent: OpponentStatus,
     events: List<Event>
-) : RawState(robot, status, targetStatus, events), IState {
+) : RawState(robot, status, opponent, events), IState {
     companion object : IIndexableStatic {
         val energyLevels = sequence {
             var value = 1
@@ -122,34 +120,31 @@ class State(
             }
             yield(value * Rules.MAX_VELOCITY)
         }.toList()
-        val maxBrakeDistance = Rules.MAX_VELOCITY * (Rules.MAX_VELOCITY / Rules.DECELERATION) / 2
-        private val maxValues: List<Int> = listOf(energyLevels.size + 1, 3, 2, 2, distanceLevels.size + 1, 4, energyLevels.size + 1, 4, 3)
+        private val maxValues: List<Int> = listOf(energyLevels.size + 1, 3, 2, energyLevels.size + 1, 4, 3, 2, distanceLevels.size + 1, 4, 2)
         override val maxIndex: Int = maxValues.product()
     }
 
     val bearing: PolarPoint = (opponent.position - status.position).toPolarPoint()
 
-    fun radianToOpponent(radian: Double) = normalizeRadian(bearing.radian - radian, true)
-
     /**
-     * |energy|velocity|gunheat|hitwall|dist|bearing|op.energy|op.heading|op.velocity|
-     * |  4   |   3    |   2   |   2   |  4 |   4   |    4    |    4     |     3     |
+     * |energy|velocity|hitwall|op.energy|op.rheading|op.velocity|op.hitwall|dist|bearing|gunheat|
+     * |  4   |   3    |   2   |    4    |     4     |     3     |    2     |  4 |   4   |   2   |
      */
     override val index: Int
-        get() {
-            val energyLevel = energyLevels.indexOfFirst { status.energy < it }.let { if (it == -1) energyLevels.size else it }
-            val velocityFlag = if (status.velocity == 0.0) 0 else if (status.velocity == Rules.MAX_VELOCITY) 2 else 1
-            val gunHeatFlag = if (status.gunHeat == 0.0) 1 else 0
-            //val gunRangeFlag = if (abs(radianToOpponent(status.gunHeading)) <= Rules.GUN_TURN_RATE_RADIANS) 1 else 0
-            val hitWallFlag = if (events.any { it is HitWallEvent }) 1 else 0
-            // Direction.entries.firstOrNull { distanceToWall(it) < maxBrakeDistance }?.ordinal ?: 0
-            val distanceLevel = distanceLevels.indexOfFirst { bearing.length < it }.let { if (it == -1) distanceLevels.size else it }
-            val bearingLevel = floor(radianToOpponent(status.heading) / Math.PI * 2).toInt()
-            val opEnergyLevel = energyLevels.indexOfFirst { opponent.energy < it }.let { if (it == -1) energyLevels.size else it }
-            val opHeadingLevel = floor((opponent.heading - status.heading) / Math.PI * 2).toInt()
-            val opVelocityFlag = if (opponent.velocity == 0.0) 0 else if (opponent.velocity == Rules.MAX_VELOCITY) 2 else 1
-            return encodeIndex(listOf(energyLevel, velocityFlag, gunHeatFlag, hitWallFlag, distanceLevel, bearingLevel, opEnergyLevel, opHeadingLevel, opVelocityFlag), maxValues)
-        }
+        get() = encodeIndex(
+            listOf(
+                energyLevels.indexOfFirst { status.energy < it }.let { if (it == -1) energyLevels.size else it },
+                if (status.velocity == 0.0) 0 else if (status.velocity == Rules.MAX_VELOCITY) 2 else 1,
+                events.any { it is HitWallEvent }.toInt(),
+                energyLevels.indexOfFirst { opponent.energy < it }.let { if (it == -1) energyLevels.size else it },
+                floor(normalizeRadian(opponent.heading - status.heading, true) / Math.PI * 2).toInt(),
+                if (opponent.velocity == 0.0) 0 else if (opponent.velocity == Rules.MAX_VELOCITY) 2 else 1,
+                distanceToWalls(opponent.position).run { up <= 0.0 || down <= 0.0 || left <= 0.0 || right <= 0.0 }.toInt(),
+                distanceLevels.indexOfFirst { bearing.length < it }.let { if (it == -1) distanceLevels.size else it },
+                floor(normalizeRadian(bearing.radian - status.heading, true) / Math.PI * 2).toInt(),
+                (status.gunHeat == 0.0).toInt()
+            ), maxValues
+        )
 }
 
 data class OpponentStatus(
@@ -160,9 +155,9 @@ data class OpponentStatus(
     val velocity: Double
 )
 
-enum class Direction {
-    Up,
-    Down,
-    Left,
-    Right
-}
+data class WallDistance(
+    val up: Double,
+    val down: Double,
+    val left: Double,
+    val right: Double
+)
