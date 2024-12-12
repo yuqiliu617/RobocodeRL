fd4297d5562be5445a7fb9598955586f92d12cac
Yuqi Liu <yuqiliu617@outlook.com>
2024-12-12 06:55:24 -0800

    feat: change Action presentation
    Add SteerLeftHalf and SteerRightHalf.

--- src/main/kotlin/org/yuqi/robot/bridge/Action.kt
+++ src/main/kotlin/org/yuqi/robot/bridge/Action.kt
@@ -15,12 +15,9 @@ enum class Action(val component: ActionComponent?, val value: Double) : IIndexab
     Accelerate(ActionComponent.Engine, 1.0),
     Decelerate(ActionComponent.Engine, -1.0),
     SteerLeft(ActionComponent.SteeringGear, Rules.MAX_TURN_RATE_RADIANS),
+    SteerLeftHalf(ActionComponent.SteeringGear, Rules.MAX_TURN_RATE_RADIANS / 2),
     SteerRight(ActionComponent.SteeringGear, -Rules.MAX_TURN_RATE_RADIANS),
-
-    /*TurretLeftHalf(ActionComponent.Turret, Rules.GUN_TURN_RATE_RADIANS / 2),
-    TurretRightHalf(ActionComponent.Turret, -Rules.GUN_TURN_RATE_RADIANS / 2),
-    TurretLeftFull(ActionComponent.Turret, Rules.GUN_TURN_RATE_RADIANS),
-    TurretRightFull(ActionComponent.Turret, -Rules.GUN_TURN_RATE_RADIANS),*/
+    SteerRightHalf(ActionComponent.SteeringGear, -Rules.MAX_TURN_RATE_RADIANS / 2),
     FireMinPower(ActionComponent.Trigger, Rules.MIN_BULLET_POWER),
     FireMedPower(ActionComponent.Trigger, 1.0),
     FireHighPower(ActionComponent.Trigger, 2.0),
@@ -38,9 +35,10 @@ enum class Action(val component: ActionComponent?, val value: Double) : IIndexab
         if (component == null)
             return
         when (component) {
-            ActionComponent.Engine       -> setAhead(if (value == 0.0) 0.0 else value.sign * Double.MAX_VALUE)
+            ActionComponent.Engine       -> setAhead(if (abs(value) < 0.1) 0.0 else value.sign * Double.MAX_VALUE)
             ActionComponent.SteeringGear -> setTurnLeftRadians(value.sign * min(abs(value), Rules.MAX_TURN_RATE_RADIANS))
             ActionComponent.Turret       -> setTurnGunLeftRadians(value.sign * min(abs(value), Rules.GUN_TURN_RATE_RADIANS))
+            ActionComponent.Radar        -> setTurnRadarLeftRadians(value.sign * min(abs(value), Rules.RADAR_TURN_RATE_RADIANS))
             ActionComponent.Trigger      -> {
                 if (gunHeat == 0.0)
                     setFire(min(Rules.MAX_BULLET_POWER, max(Rules.MIN_BULLET_POWER, value)))
@@ -53,5 +51,6 @@ enum class ActionComponent {
     Engine,
     SteeringGear,
     Turret,
+    Radar,
     Trigger
 }
